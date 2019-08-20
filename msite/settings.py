3c78b6c274a0e7a5968bee5c7afe85c36d895960
manishwg <manishwg@gmail.com>
2019-08-20 10:55:00 +0530

    additional files and changes for Heroku

--- msite/settings.py
+++ msite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,14 +129,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print('STATIC_ROOT')
 print(STATIC_ROOT)
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
-
 
 
 
