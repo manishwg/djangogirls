82cfeee0643f7e75881afd0aa714280ac0d5cb47
manishwg <manishwg@gmail.com>
2019-08-20 10:45:23 +0530

    additional files and changes for Heroku

--- msite/settings.py
+++ msite/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'msite.urls'
@@ -136,11 +135,6 @@ STATICFILES_DIRS = (
 )
 
 
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
